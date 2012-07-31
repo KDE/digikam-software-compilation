504b014631568b75210dbc65f04c2b2b23694e10
Gilles Caulier <caulier.gilles@gmail.com>
2012-07-31 18:06:47 +0200

    update common script with victor file

--- cmake/modules/FindKipi.cmake
+++ cmake/modules/FindKipi.cmake
@@ -1,116 +1,104 @@
-# - Try to find the Kipi library
+# Module that tries to find the Kipi library
 #
 # If you have put a local version of libkipi into your source tree,
 # set KIPI_LOCAL_DIR to the relative path to the local directory.
 #
 # Once done this will define
 #
-#  KIPI_FOUND - system has libkipi
+#  KIPI_FOUND       - system has libkipi
 #  KIPI_INCLUDE_DIR - the libkipi include directory
-#  KIPI_LIBRARIES - Link these to use libkipi
+#  KIPI_LIBRARIES   - Link these to use libkipi
 #  KIPI_DEFINITIONS - Compiler switches required for using libkipi
+#  KIPI_VERSION     - The version of the Kipi library
 #
 
-# Copyright (c) 2008-2011, Gilles Caulier, <caulier.gilles@gmail.com>
+# Copyright (c) 2012, Victor Dodon <dodonvictor at gmail dot com>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-if (KIPI_INCLUDE_DIR AND KIPI_LIBRARIES AND KIPI_DEFINITIONS)
+IF(KIPI_INCLUDE_DIR AND KIPI_LIBRARIES AND KIPI_DEFINITIONS AND KIPI_VERSION)
 
-  message(STATUS "Found Kipi library in cache: ${KIPI_LIBRARIES}")
+  MESSAGE(STATUS "Found kipi library in cache ${KIPI_LIBRARIES}")
+  # Already in cache
+  SET(KIPI_FOUND TRUE)
 
-  # in cache already
-  set(KIPI_FOUND TRUE)
+ELSE(KIPI_INCLUDE_DIR AND KIPI_LIBRARIES AND KIPI_DEFINITIONS AND KIPI_VERSION)
 
-else (KIPI_INCLUDE_DIR AND KIPI_LIBRARIES AND KIPI_DEFINITIONS)
+  MESSAGE(STATUS "Check Kipi library in local sub-folder...")
 
-  message(STATUS "Check Kipi library in local sub-folder...")
+  IF(KIPI_LOCAL_DIR)
+    SET(KIPI_LOCAL_FOUND TRUE)
+  ELSE(KIPI_LOCAL_DIR)
+    FIND_FILE(KIPI_LOCAL_FOUND libkipi/plugin.h ${CMAKE_SOURCE_DIR}/libkipi
+      ${CMAKE_SOURCE_DIR}/libs/libkipi NO_DEFAULT_PATH)
 
-  # Check if library is not in local sub-folder
+    IF (KIPI_LOCAL_FOUND)
+      FIND_FILE(KIPI_LOCAL_FOUND_IN_LIBS libkipi/plugin.h
+        ${CMAKE_SOURCE_DIR}/libs/libkipi NO_DEFAULT_PATH)
+      IF(KIPI_LOCAL_FOUND_IN_LIBS)
+        SET(KIPI_LOCAL_DIR libs/libkipi)
+      ELSE(KIPI_LOCAL_FOUND_IN_LIBS)
+        SET(KIPI_LOCAL_DIR libkipi)
+      ENDIF(KIPI_LOCAL_FOUND_IN_LIBS)
+    ENDIF(KIPI_LOCAL_FOUND)
+  ENDIF(KIPI_LOCAL_DIR)
 
-  if (KIPI_LOCAL_DIR)
-    set (KIPI_LOCAL_FOUND TRUE)
-  else (KIPI_LOCAL_DIR)
-    find_file(KIPI_LOCAL_FOUND libkipi/kipi.h ${CMAKE_SOURCE_DIR}/libkipi ${CMAKE_SOURCE_DIR}/libs/libkipi NO_DEFAULT_PATH)
+  IF(KIPI_LOCAL_FOUND)
 
-    if (KIPI_LOCAL_FOUND)
-      # Was it found in libkipi/ or in libs/libkipi?
-      find_file(KIPI_LOCAL_FOUND_IN_LIBS libkipi/kipi.h ${CMAKE_SOURCE_DIR}/libs/libkipi NO_DEFAULT_PATH)
-      if (KIPI_LOCAL_FOUND_IN_LIBS)
-        set(KIPI_LOCAL_DIR libs/libkipi)
-      else (KIPI_LOCAL_FOUND_IN_LIBS)
-        set(KIPI_LOCAL_DIR libkipi)
-      endif (KIPI_LOCAL_FOUND_IN_LIBS)
-    endif (KIPI_LOCAL_FOUND)
-  endif (KIPI_LOCAL_DIR)
+    SET(KIPI_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/${KIPI_LOCAL_DIR}
+      ${CMAKE_BINARY_DIR}/${KIPI_LOCAL_DIR})
+    SET(KIPI_DEFINITIONS "-I${CMAKE_SOURCE_DIR}/${KIPI_LOCAL_DIR}"
+      "-I${CMAKE_BINARY_DIR}/${KIPI_LOCAL_DIR}")
+    SET(KIPI_LIBRARIES kipi)
+    MESSAGE(STATUS "Found Kipi library in local sub-folder: ${CMAKE_SOURCE_DIR}/${KIPI_LOCAL_DIR}")
 
-  if (KIPI_LOCAL_FOUND)
-    # we need two include directories: because the version.h file is put into the build directory
-    # TODO KIPI_INCLUDE_DIR sounds like it should contain only one directory...
-    set(KIPI_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/${KIPI_LOCAL_DIR} ${CMAKE_BINARY_DIR}/${KIPI_LOCAL_DIR})
-    set(KIPI_DEFINITIONS "-I${CMAKE_SOURCE_DIR}/${KIPI_LOCAL_DIR}" "-I${CMAKE_BINARY_DIR}/${KIPI_LOCAL_DIR}")
-    set(KIPI_LIBRARIES kipi)
-    message(STATUS "Found Kipi library in local sub-folder: ${CMAKE_SOURCE_DIR}/${KIPI_LOCAL_DIR}")
-    set(KIPI_FOUND TRUE)
-    mark_as_advanced(KIPI_INCLUDE_DIR KIPI_LIBRARIES KIPI_DEFINITIONS)
+    SET(KIPI_FOUND TRUE)
 
-  else (KIPI_LOCAL_FOUND)
+    SET(KIPI_CMAKE_FILE ${CMAKE_SOURCE_DIR}/${KIPI_LOCAL_DIR}/CMakeLists.txt)
+    FILE(READ ${KIPI_CMAKE_FILE} KIPI_CMAKE_FILE_CONTENT)
+    STRING(REGEX REPLACE ".*[\n|^]SET[ \t]*[(]KIPI_LIB_MAJOR_VERSION \"([0-9]+)\"[)][ \t]*[\n|$].*"
+      "\\1" KIPI_MAJOR_VERSION "${KIPI_CMAKE_FILE_CONTENT}")
+    STRING(REGEX REPLACE ".*[\n|^]SET[ \t]*[(]KIPI_LIB_MINOR_VERSION \"([0-9]+)\"[)][ \t]*[\n|$].*"
+      "\\1" KIPI_MINOR_VERSION "${KIPI_CMAKE_FILE_CONTENT}")
+    STRING(REGEX REPLACE ".*[\n|^]SET[ \t]*[(]KIPI_LIB_PATCH_VERSION \"([0-9]+)\"[)][ \t]*[\n|$].*"
+      "\\1" KIPI_PATCH_VERSION "${KIPI_CMAKE_FILE_CONTENT}")
+    STRING(REGEX REPLACE ".*[\n|^]SET[ \t]*[(]KIPI_LIB_SUFFIX_VERSION \"([^\"]*)\"[)][ \t]*[\n|$].*"
+      "\\1" KIPI_SUFFIX_VERSION "${KIPI_CMAKE_FILE_CONTENT}")
+    SET(KIPI_VERSION "${KIPI_MAJOR_VERSION}.${KIPI_MINOR_VERSION}.${KIPI_PATCH_VERSION}${KIPI_SUFFIX_VERSION}")
 
-    if (NOT WIN32)
-      message(STATUS "Check Kipi library using pkg-config...")
+  ELSE(KIPI_LOCAL_FOUND)
 
-      # use pkg-config to get the directories and then use these values
-      # in the FIND_PATH() and FIND_LIBRARY() calls
-      include(UsePkgConfig)
+    IF(NOT WIN32)
+      INCLUDE(FindPkgConfig)
+      PKG_CHECK_MODULES(KIPI libkipi>=2.0.0)
+    ENDIF(NOT WIN32)
 
-      PKGCONFIG(libkipi _KIPIIncDir _KIPILinkDir _KIPILinkFlags _KIPICflags)
+    FIND_LIBRARY(KIPI_LIBRARIES
+      NAMES
+      libkipi
+      PATHS
+      ${KIPI_LIBRARY_DIRS}
+      ${LIB_INSTALL_DIR}
+      ${KDE4_LIB_DIR}
+    )
 
-      if (_KIPILinkFlags)
-        # query pkg-config asking for a libkipi >= 1.0.0
-        exec_program(${PKGCONFIG_EXECUTABLE} ARGS --atleast-version=1.0.0 libkipi RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
-        if (_return_VALUE STREQUAL "0")
-            message(STATUS "Found libkipi release >= 1.0.0")
-            set(KIPI_VERSION_GOOD_FOUND TRUE)
-        else (_return_VALUE STREQUAL "0")
-            message(STATUS "Found libkipi release < 1.0.0, too old")
-            set(KIPI_VERSION_GOOD_FOUND FALSE)
-            set(KIPI_FOUND FALSE)
-        endif (_return_VALUE STREQUAL "0")
-      else (_KIPILinkFlags)
-        set(KIPI_VERSION_GOOD_FOUND FALSE)
-        set(KIPI_FOUND FALSE)
-      endif (_KIPILinkFlags)
-    else (NOT WIN32)
-      set(KIPI_VERSION_GOOD_FOUND TRUE)
-    endif (NOT WIN32)
-    if (KIPI_VERSION_GOOD_FOUND)
-        set(KIPI_DEFINITIONS ${_KIPICflags})
+    FIND_PATH(KIPI_INCLUDE_DIR
+      NAMES
+      libkipi/version.h
+      PATHS
+      ${KIPI_INCLUDE_DIRS}
+      ${INCLUDE_INSTALL_DIR}
+      ${KDE4_INCLUDE_DIR}
+    )
 
-        find_path(KIPI_INCLUDE_DIR NAMES libkipi/version.h PATHS ${KDE4_INCLUDE_DIR} ${_KIPIIncDir})
-        find_library(KIPI_LIBRARIES NAMES kipi PATHS ${KDE4_LIB_DIR} ${_KIPILinkDir})
+    SET(KIPI_DEFINITIONS ${KIPI_CFLAGS})
 
-        if (KIPI_INCLUDE_DIR AND KIPI_LIBRARIES)
-            set(KIPI_FOUND TRUE)
-        endif (KIPI_INCLUDE_DIR AND KIPI_LIBRARIES)
-      endif (KIPI_VERSION_GOOD_FOUND)
-      if (KIPI_FOUND)
-          if (NOT Kipi_FIND_QUIETLY)
-              message(STATUS "Found libkipi: ${KIPI_LIBRARIES}")
-          endif (NOT Kipi_FIND_QUIETLY)
-      else (KIPI_FOUND)
-          if (Kipi_FIND_REQUIRED)
-              if (NOT KIPI_INCLUDE_DIR)
-                  message(FATAL_ERROR "Could NOT find libkipi header files")
-              endif (NOT KIPI_INCLUDE_DIR)
-              if (NOT KIPI_LIBRARIES)
-                  message(FATAL_ERROR "Could NOT find libkipi library")
-              endif (NOT KIPI_LIBRARIES)
-          endif (Kipi_FIND_REQUIRED)
-      endif (KIPI_FOUND)
+    INCLUDE(FindPackageHandleStandardArgs)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(KIPI DEFAULT_MSG KIPI_LIBRARIES KIPI_INCLUDE_DIR)
 
-    mark_as_advanced(KIPI_INCLUDE_DIR KIPI_LIBRARIES KIPI_DEFINITIONS)
+  ENDIF(KIPI_LOCAL_FOUND)
 
-  endif (KIPI_LOCAL_FOUND)
+  MARK_AS_ADVANCED(KIPI_INCLUDE_DIR KIPI_LIBRARIES KIPI_DEFINITIONS KIPI_VERSION)
 
-endif (KIPI_INCLUDE_DIR AND KIPI_LIBRARIES AND KIPI_DEFINITIONS)
+ENDIF(KIPI_INCLUDE_DIR AND KIPI_LIBRARIES AND KIPI_DEFINITIONS AND KIPI_VERSION)
