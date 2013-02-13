b3af2867dd186ca78735c17841cbe5b064863c96
Gilles Caulier <caulier.gilles@gmail.com>
2013-02-13 12:54:09 +0100

    update

--- project/reports/coverity.sh
+++ project/reports/coverity.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# See this url to see how to prepare your computer with Coverity SCAN tool:
+# http://scan.coverity.com/self-build/
+
 cd ../../build/
 cov-build --dir cov-int --tmpdir ~/tmp make
 tar czvf myproject.tgz cov-int
+
+echo "Coverity Scan tarball 'myproject.tgz' ready to be uploaded at http://scan.coverity.com/upload.html fpr analyse"
