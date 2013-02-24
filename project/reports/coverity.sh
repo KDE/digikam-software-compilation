5263cb18fa685f793b03f4d3ad837a2a1e8ec5c4
Gilles Caulier <caulier.gilles@gmail.com>
2013-02-24 13:32:32 +0100

    update

--- project/reports/coverity.sh
+++ project/reports/coverity.sh
@@ -7,4 +7,15 @@ cd ../../build/
 cov-build --dir cov-int --tmpdir ~/tmp make
 tar czvf myproject.tgz cov-int
 
-echo "Coverity Scan tarball 'myproject.tgz' ready to be uploaded at http://scan.coverity.com/upload.html fpr analyse"
+echo "Coverity Scan tarball 'myproject.tgz' uploading in progress..."
+
+curl --form file=@myproject.tgz \
+     --form project=digiKam \
+     --form password=$digiKamCoverityPassword \
+     --form email=caulier.gilles@gmail.com \
+     --form version=git-master \
+     --form description="Git/Master from KDE repository" \
+     http://scan5.coverity.com/cgi-bin/upload.py
+
+echo "Coverity Scan tarball 'myproject.tgz' is uploaded and ready for analyse."
+
