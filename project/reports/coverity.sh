772b26b63a202f30a53718f4860cb1f0241ae771
Gilles Caulier <caulier.gilles@gmail.com>
2013-03-19 11:28:58 +0100

    prepare SCAN import to handle git active branches in decription string

--- project/reports/coverity.sh
+++ project/reports/coverity.sh
@@ -19,6 +19,14 @@ fi
 
 ./bootstrap.linux
 
+# Get active git branches to create SCAN import description string
+./gits branch | sed -e "s/*/#/g" | sed -e "s/On:/#On:/g" | grep "#" | sed -e "s/#On:/On:/g" | sed -e "s/#/BRANCH:/g" > ./build/git_branches.txt
+desc=$(<build/git_branches.txt)
+
+echo "-- SCAN Import description --"
+echo $desc
+echo "--"
+
 cd ./build
 
 cov-build --dir cov-int --tmpdir ~/tmp make
@@ -35,7 +43,7 @@ curl -v \
      --form password=$digiKamCoverityPassword \
      --form email=caulier.gilles@gmail.com \
      --form version=git-master \
-     --form description="Git/Master from KDE repository" \
+     --form description=$desc \
      http://scan5.coverity.com/cgi-bin/upload.py
 
 echo "Done. Coverity Scan tarball 'myproject.tgz' is uploaded and ready for analyse."
