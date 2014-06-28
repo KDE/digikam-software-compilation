9dabd9a24ac85f52f2b0255a6c891b15e676bdb5
Gilles Caulier <caulier.gilles@gmail.com>
2014-06-28 08:02:02 +0200

    polish

--- bootstrap.msvc.bat
+++ bootstrap.msvc.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-REM  Copyright (c) 2008-2012, Gilles Caulier, <caulier dot gilles at gmail dot com>
+REM  Copyright (c) 2008-2014, Gilles Caulier, <caulier dot gilles at gmail dot com>
 REM 
 REM  Redistribution and use is allowed according to the terms of the BSD license.
 REM  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
@@ -27,10 +27,11 @@ REM Microsoft Visual C++ command line compiler.
 cmake -G "NMake Makefiles" . ^
       -DCMAKE_BUILD_TYPE=relwithdebinfo ^
       -DCMAKE_INSTALL_PREFIX=%KDE4_INSTALL_DIR% ^
-      -DKDE4_BUILD_TESTS=on ^
-      -DDIGIKAMSC_USE_PRIVATE_KDEGRAPHICS=on ^
-      -DDIGIKAMSC_COMPILE_DOC=off ^
-      -DENABLE_LCMS2=off ^
+      -DKDE4_BUILD_TESTS=ON ^
+      -DDIGIKAMSC_USE_PRIVATE_KDEGRAPHICS=ON ^
+      -DDIGIKAMSC_COMPILE_DOC=OFF ^
+      -DENABLE_LCMS2=OFF ^
+      -DENABLE_INTERNALMYSQL=OFF ^ 
       -Wno-dev ^
       -DEXPAT_LIBRARY=%KDE4_INSTALL_DIR%/lib/expat.lib ^
       -DCMAKE_INCLUDE_PATH=%KDE4_INSTALL_DIR%/include ^
