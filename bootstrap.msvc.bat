0d8e7c65fcec52c2b28a4ed3fae39eb845e1f75d
Gilles Caulier <caulier.gilles@gmail.com>
2014-07-04 10:09:09 +0200

    fix space at EOL

--- bootstrap.msvc.bat
+++ bootstrap.msvc.bat
@@ -32,7 +32,7 @@ cmake -G "NMake Makefiles" . ^
       -DDIGIKAMSC_COMPILE_PO=OFF ^
       -DDIGIKAMSC_COMPILE_DOC=OFF ^
       -DENABLE_LCMS2=OFF ^
-      -DENABLE_INTERNALMYSQL=OFF ^ 
+      -DENABLE_INTERNALMYSQL=OFF ^
       -Wno-dev ^
       -DEXPAT_LIBRARY=%KDE4_INSTALL_DIR%/lib/expat.lib ^
       -DCMAKE_INCLUDE_PATH=%KDE4_INSTALL_DIR%/include ^
