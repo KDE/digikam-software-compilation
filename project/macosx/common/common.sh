551bf1dc0085d056069eff4b85c4b16aa92c19dc
Gilles Caulier <caulier.gilles@gmail.com>
2015-05-28 14:35:48 +0200

    polish

--- project/macosx/common/common.sh
+++ project/macosx/common/common.sh
@@ -93,7 +93,7 @@ TerminateScript()
 
 termin=$(date +"%s")
 difftimelps=$(($termin-$begin))
-echo "$(($difftimelps / 60)) minutes and $(($difftimelps % 60)) seconds elapsed for Script execution."
+echo "$(($difftimelps / 60)) minutes and $(($difftimelps % 60)) seconds elapsed for script execution."
 
 }
 
