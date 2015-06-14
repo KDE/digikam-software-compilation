0af6012d43aed806b7545698c153cc8650b7cb08
Gilles Caulier <caulier.gilles@gmail.com>
2015-06-14 15:23:13 +0200

    polish

--- project/macosx/common/common.sh
+++ project/macosx/common/common.sh
@@ -16,7 +16,7 @@ INSTALL_PREFIX="/opt/digikam"
 # Libraries to build outside Macports at the same time than digiKam (0: use port file / 1: use tarball)
 ENABLE_LIBRAW=1
 ENABLE_EXIV2=1
-ENABLE_LENSFUN=1
+ENABLE_LENSFUN=0
 
 # digiKam version to build
 DK_VERSION="4.10.0"
