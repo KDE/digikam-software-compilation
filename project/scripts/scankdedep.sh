fcba00e7e03f4c5dfd6aaccf0ce95ee46cea6d32
Gilles Caulier <caulier.gilles@gmail.com>
2016-04-05 13:43:17 +0159

    update

--- project/scripts/scankdedep.sh
+++ project/scripts/scankdedep.sh
@@ -2,8 +2,6 @@
 
 grep --exclude-dir PORT.KF5 -r "#include <k" * | \
    grep -v "#include <klocalizedstring.h>" | \
-   grep -v "exiv"                          | \
-   grep -v "dcraw"                         | \
    grep -v "config"                        | \
    grep -v "ipi"                           | \
    grep -v "action"
