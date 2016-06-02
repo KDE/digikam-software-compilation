99e49e11b87248c9b67db9bba61324417e4083fd
Gilles Caulier <caulier.gilles@gmail.com>
2016-06-02 20:45:36 +0200

    polish

--- project/mxe/makeall.sh
+++ project/mxe/makeall.sh
@@ -5,21 +5,25 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
+. ./config.sh
+StartScript
+
 # halt on error
 set -e
 
 echo "This script will build from scratch the digiKam installer for Windows using MXE."
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
-if [ -d "./build" ] ; then
+if [ -d "$MXE_BUILDROOT" ] ; then
 
     read -p "A previous MXE build already exist and it will be removed. Do you want to continue ? [(c)ontinue/(s)top] " answer
 
     if echo "$answer" | grep -iq "^c" ;then
 
         echo "---------- Removing existing MXE build"
-        chmod +w "./build/usr/readonly"
-        chattr -i "./build/usr/readonly/.gitkeep"
-        rm -fr ./build
+        chmod +w "$MXE_BUILDROOT/usr/readonly"
+        chattr -i "$MXE_BUILDROOT/usr/readonly/.gitkeep"
+        rm -fr $MXE_BUILDROOT
 
     else
 
@@ -34,3 +38,7 @@ fi
 ./02-build-extralibs.sh
 ./03-build-digikam.sh
 ./04-build-installer.sh
+
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+TerminateScript
