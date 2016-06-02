937afab51503f250c4ad212ba880558a0e205422
Gilles Caulier <caulier.gilles@gmail.com>
2016-06-02 08:42:46 +0159

    polish

--- project/release/fetch_l10n_doc.rb
+++ project/release/fetch_l10n_doc.rb
@@ -7,7 +7,7 @@
 #
 # Copyright (c)      2005, Mark Kretschmann, <kretschmann at kde dot org>
 # Copyright (c)      2014, Nicolas Lécureuil, <kde at nicolaslecureuil dot fr>
-# Copyright (c) 2010-2015, Gilles Caulier, <caulier dot gilles at gmail dot com>
+# Copyright (c) 2010-2016, Gilles Caulier, <caulier dot gilles at gmail dot com>
 #
 # Some parts of this code taken from cvs2dist
 # License: GNU General Public License V2
@@ -35,6 +35,7 @@ end
 #protocol = `kdialog --radiolist "Do you use https or svn+ssh?" https https 0 "svn+ssh" "svn+ssh" 1`.chomp()
 
 i18nlangs = []
+
 if isWindows
     i18nlangs = `type .\\project\\release\\subdirs`
 else
@@ -50,14 +51,19 @@ end
 
 Dir.chdir( "doc-translated" )
 topmakefile = File.new( "CMakeLists.txt", File::CREAT | File::RDWR | File::TRUNC )
+
 i18nlangs.each_line do |lang|
     lang.chomp!()
+
     if (lang != nil && lang != "")
         print("#{lang} ")
+
         if !(File.exists?(lang) && File.directory?(lang))
             Dir.mkdir(lang)
         end
+
         Dir.chdir(lang)
+
         for part in ['color-management', 'credits-annex', 'editor-color', 'editor-decorate', 'editor-enhance', 'editor-filters', 'editor-transform', 'file-formats', 'ie-menu', 'index', 'menu-descriptions', 'photo-editing', 'sidebar']
             #puts "Copying #{lang}'s #{part}.docbook over..  "
             if isWindows
--- project/release/fetch_l10n_po.rb
+++ project/release/fetch_l10n_po.rb
@@ -7,7 +7,7 @@
 #
 # Copyright (c)      2005, Mark Kretschmann, <kretschmann at kde dot org>
 # Copyright (c)      2014, Nicolas Lécureuil, <kde at nicolaslecureuil dot fr>
-# Copyright (c) 2010-2015, Gilles Caulier, <caulier dot gilles at gmail dot com>
+# Copyright (c) 2010-2016, Gilles Caulier, <caulier dot gilles at gmail dot com>
 #
 # Some parts of this code taken from cvs2dist
 # License: GNU General Public License V2
@@ -35,6 +35,7 @@ end
 #protocol = `kdialog --radiolist "Do you use https or svn+ssh?" https https 0 "svn+ssh" "svn+ssh" 1`.chomp()
 
 i18nlangs = []
+
 if isWindows
     i18nlangs = `type .\\project\\release\\subdirs`
 else
@@ -42,21 +43,29 @@ else
 end
 
 ##########################################################################################
-#EXTRACT TRANSLATED APPLICATION FILES
+# EXTRACT TRANSLATED APPLICATION FILES
 
 if !(File.exists?("po") && File.directory?("po"))
     Dir.mkdir( "po" )
 end
+
 Dir.chdir( "po" )
 topmakefile = File.new( "CMakeLists.txt", File::CREAT | File::RDWR | File::TRUNC )
+
 i18nlangs.each_line do |lang|
+
     lang.chomp!()
+
     if (lang != nil && lang != "")
+
         print ("#{lang} ")
+
         if !(File.exists?(lang) && File.directory?(lang))
             Dir.mkdir(lang)
         end
+
         Dir.chdir(lang)
+
         for part in ['digikam','kipiplugin_facebook','kipiplugin_flashexport','kipiplugin_flickr','kipiplugin_remotestorage','kipiplugin_googledrive','kipiplugin_piwigo','kipiplugin_printimages','kipiplugin_sendimages','kipiplugin_smug','kipiplugins','kipiplugin_dropbox','kipiplugin_imageshack','kipiplugin_imgur','kipiplugin_kmlexport', 'kipiplugin_rajce','kipiplugin_vkontakte','kipiplugin_wikimedia','kipiplugin_yandexfotki']
 
             #puts "Copying #{lang}'s #{part} over..  "
