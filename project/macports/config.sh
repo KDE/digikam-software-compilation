975cca61e43b28cdb07920128540f45d981f8380
Gilles Caulier <caulier.gilles@gmail.com>
2016-07-23 14:17:21 +0200

    update instructions about git tags to use

--- project/macports/config.sh
+++ project/macports/config.sh
@@ -74,9 +74,12 @@ KA_VERSION=16.04.3
 DK_URL="http://download.kde.org/stable/digikam"
 DK_BUILDTEMP=~/dktemp
 # digiKam tag version from git. Official tarball do not include extra shared libraries.
-DK_VERSION=v5.0.0
+# The list of tags can be listed with this url: https://quickgit.kde.org/?p=digikam.git&a=tags
+# If you want to package current implemntation from git, use "master" as tag.
+#DK_VERSION=v5.0.0
+DK_VERSION=master
 # Installer sub version to differentiates newer updates of the installer itself, even if the underlying application hasn’t changed.
-DK_EPOCH="-2"
+DK_EPOCH="-1"
 
 # Hugin tarball information
 HU_URL="http://sourceforge.net/projects/hugin/files/hugin/"
