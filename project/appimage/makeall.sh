053e8a32c2f7b603198ecc9f2462c7ac9e062ac1
Gilles Caulier <caulier.gilles@gmail.com>
2016-10-17 10:15:04 -0400

    add new script to run all from scratch

---
+++ project/appimage/makeall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Script to run all Linux sub-scripts to build AppImage bundle.
+#
+# Copyright (c) 2015-2016, Gilles Caulier, <caulier dot gilles at gmail dot com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+
+# halt on error
+set -e
+
+#################################################################################################
+# Pre-processing checks
+
+. ./config.sh
+. ./common.sh
+StartScript
+ChecksRunAsRoot
+ChecksCPUCores
+CentOS6Adjustments
+
+echo "This script will build from scratch the digiKam AppImage bundle for Linux."
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+./01-build-centos6.sh
+./02-build-extralibs.sh
+./03-build-digikam.sh
+./04-build-appimage.sh
+
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+TerminateScript
