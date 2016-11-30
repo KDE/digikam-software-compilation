e820d4dae66994026b337fcf1a1ccf57e605eca5
Gilles Caulier <caulier.gilles@gmail.com>
2016-11-30 07:48:13 +0100

    new script to update installers

---
+++ project/bundles/mxe/update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Script to update digiKam installer.
+#
+# Copyright (c) 2013-2016, Gilles Caulier, <caulier dot gilles at gmail dot com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+
+# halt on error
+set -e
+
+. ./config.sh
+. ./common.sh
+StartScript
+
+echo "++++++++++++++++   Build 32 bits Installer   ++++++++++++++++++++++++++++++++++"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+sed -e "s/MXE_ARCHBITS=64/MXE_ARCHBITS=32/g" ./config.sh > ./tmp.sh ; mv -f ./tmp.sh ./config.sh
+
+./03-build-digikam.sh
+./04-build-installer.sh
+
+echo "++++++++++++++++   Build 64 bits Installer   ++++++++++++++++++++++++++++++++++"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+sed -e "s/MXE_ARCHBITS=32/MXE_ARCHBITS=64/g" ./config.sh > ./tmp.sh ; mv -f ./tmp.sh ./config.sh
+
+./03-build-digikam.sh
+./04-build-installer.sh
+
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+TerminateScript
