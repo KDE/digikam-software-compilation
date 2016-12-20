65a239da5c70794f3146a2924ba56363122677e4
Gilles Caulier <caulier.gilles@gmail.com>
2016-12-20 13:11:11 +0100

    default variant is enough : FFMpeg GPL2 codecs only

--- project/bundles/macports/01-build-macports.sh
+++ project/bundles/macports/01-build-macports.sh
@@ -245,7 +245,7 @@ port install qt5 \
              libxslt \
              lensfun \
              bison \
-             ffmpeg-devel +gpl2 +gpl3 +nonfree
+             ffmpeg-devel
 
 echo -e "\n"
 
