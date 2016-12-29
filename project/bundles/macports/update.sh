8d06baead34997497c1b7d4e362172a74128854e
Gilles Caulier <caulier.gilles@gmail.com>
2016-12-29 11:31:28 -0500

    new script to update installer only

---
+++ project/bundles/macports/update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script to update digiKam installer.
+#
+# Copyright (c) 2013-2017, Gilles Caulier, <caulier dot gilles at gmail dot com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+
+# halt on error
+set -e
+
+. ./config.sh
+. ./common.sh
+StartScript
+
+echo "++++++++++++++++++   Build MacOS Installer   ++++++++++++++++++++++++++++++++++"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+./03-build-digikam.sh
+./04-build-installer.sh
+
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+TerminateScript
