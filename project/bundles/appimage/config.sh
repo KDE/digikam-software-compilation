1439d909862a62c9600608d45abf06d2bc147490
Gilles Caulier <caulier.gilles@gmail.com>
2017-02-26 11:45:54 -0500

    update

--- project/bundles/appimage/config.sh
+++ project/bundles/appimage/config.sh
@@ -25,8 +25,8 @@ DK_URL="http://download.kde.org/stable/digikam"
 # The list of tags can be listed with this url: https://quickgit.kde.org/?p=digikam.git&a=tags
 # If you want to package current implemntation from git, use "master" as tag.
 #DK_VERSION=v5.2.0
-#DK_VERSION=master
-DK_VERSION=development/garbagecollection
+DK_VERSION=master
+#DK_VERSION=development/garbagecollection
 KIPI_VERSION=master
 # Installer sub version to differentiates newer updates of the installer itself, even if the underlying application hasn’t changed.
 DK_EPOCH="-01"
