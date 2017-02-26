90cc894a72b26be2dfb50f21713ced3814d5f9fb
Gilles Caulier <caulier.gilles@gmail.com>
2017-02-26 11:47:23 -0500

    update

--- project/bundles/mxe/config.sh
+++ project/bundles/mxe/config.sh
@@ -52,8 +52,8 @@ DK_BUILDTEMP=~/dktemp
 # The list of tags can be listed with this url: https://quickgit.kde.org/?p=digikam.git&a=tags
 # If you want to package current implemntation from git, use "master" as tag.
 #DK_VERSION=v5.4.0
-#DK_VERSION=master
-DK_VERSION=development/garbagecollection
+DK_VERSION=master
+#DK_VERSION=development/garbagecollection
 KIPI_VERSION=master
 # Installer sub version to differentiates newer updates of the installer itself, even if the underlying application hasn’t changed.
 DK_EPOCH="-01"
